2ed8e4d920c3dddfede9e1c9eacf7fbe23fd2ece
Sebastian Castano <sebastian9111@gmail.com>
2021-12-11 22:46:14 +0200

    add jwt not valid exception

--- src/main/java/co/edu/unbosque/clientes/ClienteController.java
+++ src/main/java/co/edu/unbosque/clientes/ClienteController.java
@@ -3,6 +3,7 @@ package co.edu.unbosque.clientes;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.security.auth.message.AuthException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -17,47 +18,53 @@ public class ClienteController {
     private final JWTChecker jwtchecker;
 
     @PostMapping
-    public void insertCliente(@RequestHeader Map<String, String> headers, @RequestBody Cliente cliente){
+    public void insertCliente(@RequestHeader Map<String, String> headers, @RequestBody Cliente cliente) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             clienteService.insertCliente(cliente);
-        }
+    }else{
+        throw new AuthException("No authorizado");
+    }
     }
 
     @GetMapping
-    public List<Cliente> getClientes(@RequestHeader Map<String, String> headers){
+    public List<Cliente> getClientes(@RequestHeader Map<String, String> headers) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             return clienteService.getClientes();
+    }else{
+        throw new AuthException("No authorizado");
         }
-        return null;
     }
 
     @GetMapping("/{id}")
-    public Optional<Cliente> getCliente(@RequestHeader Map<String, String> headers, @PathVariable int id){
+    public Optional<Cliente> getCliente(@RequestHeader Map<String, String> headers, @PathVariable int id) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             return clienteService.getCliente(id);
+        }else{
+        throw new AuthException("No authorizado");
         }
-        return null;
     }
 
     @DeleteMapping("/{id}")
-    public String deleteCliente(@RequestHeader Map<String, String> headers, @PathVariable int id){
+    public String deleteCliente(@RequestHeader Map<String, String> headers, @PathVariable int id) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             if(clienteService.deleteCliente(id)){
                 return ("{'code':'200','message': 'Registro eliminado'}");
             }
             return ("{'code': '404','message': 'Registro no encontrad'}");
+        }else{
+        throw new AuthException("No authorizado");
         }
-        return null;
     }
 
     @PutMapping("/{id}")
-    public String updateCliente(@RequestHeader Map<String, String> headers, @PathVariable int id, @RequestBody Cliente cliente){
+    public String updateCliente(@RequestHeader Map<String, String> headers, @PathVariable int id, @RequestBody Cliente cliente) throws AuthException {
         if(jwtchecker.doFilter(headers)){
             if(clienteService.updateCliente(id, cliente)){
                 return ("{'code':'200','message': 'Registro actualiazado'}");
             }
             return ("{'code': '404','message': 'Registro no encontrad'}");
+        }else{
+        throw new AuthException("No authorizado");
         }
-        return null;
     }
 }
